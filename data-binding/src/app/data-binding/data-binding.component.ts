d3cddc0e2aeb46dd47db0762a830195cfe86f5af
rivanildojr <rivanildo_junior7@hotmail.com>
2024-09-25 08:45:59 -0300

    feat: create event listeners

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -13,6 +13,9 @@ export class DataBindingComponent {
   url: string = 'https://loaiane.training.com';
   courseAngular: boolean = true;
   imageURL: string = 'http://lorempixel.com.br/500/400';
+  currentValue: string = '';
+  saveValue: string = '';
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
@@ -20,5 +23,22 @@ export class DataBindingComponent {
 
   getLikeCourse() {
     return true;
+  }
+
+  clickButton() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    console.log("🚀 ~ DataBindingComponent ~ onKeyUp ~ event:", event)
+    this.currentValue = (<HTMLInputElement>event.target).value
+  }
+
+  savedValue(value: string) {
+    this.saveValue = value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 }
