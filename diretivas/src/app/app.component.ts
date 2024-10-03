7f9dcce8de37d7551a7fe70574c331217974bd60
rivanildojr <rivanildo_junior7@hotmail.com>
2024-10-03 08:50:29 -0300

    feat: learning ngIf and control flow

--- diretivas/src/app/app.component.ts
+++ diretivas/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DiretivaNgifComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-diretiva-ngif',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './diretiva-ngif.component.html',
   styleUrl: './diretiva-ngif.component.css'
 })
 export class DiretivaNgifComponent {
+  courses: string[] = ['Angular'];
 
+  showCourse: boolean = false;
+
+  onShowCourse() {
+    this.showCourse = !this.showCourse;
+  }
 }
